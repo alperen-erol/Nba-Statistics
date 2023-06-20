c3af904d148f8176a024941748ece2ed2ef78bfe
alperen-erol <alpereneroltr@gmail.com>
2023-06-20 08:37:28 +0300

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+const Header = () => {
+  return (
+    <Container className='text-center mt-4 '>
+        <h1 className='my-2 display-2'>NBA Player Statistics</h1>
+    </Container>
+  )
+}
+
+export default Header
