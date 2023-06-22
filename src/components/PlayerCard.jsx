a19c1d3badf778f93e10809026b30f712cd3ff56
alperen-erol <alpereneroltr@gmail.com>
2023-06-22 22:48:51 +0300

    card-completed

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 
 const PlayerCard = (item) => {
-  console.log(item);
+  const statistics = item.statistics;
+  const [cardFace, setCardFace] = useState(false);
   return (
     <>
-      <Card className="rounded-1 player-card">
-        <Card.Img className="player-image" src={item.img} />
+      <Card
+        onClick={() => setCardFace(!cardFace)}
+        className="rounded-1 player-card"
+        role="button"
+      >
+        {cardFace ? (
+          <Card.Img className="player-image" src={item.img} />
+        ) : (
+          <ul className=" m-auto mt-5 statCard">
+            {statistics.map((item) => {
+              return <li className=" list-unstyled h6 text-start">{item}</li>;
+            })}
+          </ul>
+        )}
         <Card.Body>
           <Card.Title>{item.name}</Card.Title>
         </Card.Body>
